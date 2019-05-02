3e427ea16d189ada1dee72000419dc143ac4da4b
william <willef1999@gmail.com>
2019-05-02 14:55:32 +0200

    first commit

---
+++ Test/LoanTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoanTest {
+
+    @Test
+    void getAmmount() {
+    }
+
+    @Test
+    void getDuration() {
+    }
+
+    @Test
+    void getMonthly() {
+    }
+
+    @Test
+    void getTotal() {
+    }
+}
---
+++ src/Loan.java
@@ -0,0 +1,23 @@
+public class Loan {
+    private int ammount;
+    private int duration;
+    private float rate;
+    private float monthly;
+
+
+    public void getAmmount(int duration, float monthly, float rate){
+
+    }
+
+    public void getDuration(int ammount, float monthly, float rate){
+
+    }
+
+    public void getMonthly(int ammount, int duration, float rate){
+
+    }
+
+    public float getTotal(){
+        return 0f;
+    }
+}
