3d407b75e15bfd1f218b3ee3978db277404ddabf
xil <x.lamuth@gmail.com>
2019-05-02 15:57:07 +0200

    testV1

--- Test/LoanTest.java
+++ Test/LoanTest.java
@@ -4,27 +4,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LoanTest {
 
+
+    /**
+     * Test monntrant le capital emprunté
+     */
     @Test
     void getAmmountTest() {
         Loan ammount = new Loan();
-       assertEquals();
+       assertEquals(2,300,3);
     }
+
+    /**
+     * Test montrant la durée du prêt
+     */
 
     @Test
     void getDurationTest() {
         Loan duration = new Loan();
-        assertEquals();
+//        assertEquals();
     }
 
+    /**
+     * Test montrant la mensualité à payer
+     */
     @Test
     void getMonthlyTest() {
         Loan monthly = new Loan();
-        assertEquals();
+//        assertEquals();
     }
 
+    /**
+     * Test montrant le coût total du prêt
+     */
     @Test
     void getTotalTest() {
         Loan total = new Loan();
-        assertEquals();
+//        assertEquals();
     }
 }
