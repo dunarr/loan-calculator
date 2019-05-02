b054ab13d72f28c2ab847868608b762fa3599d4f
william <willef1999@gmail.com>
2019-05-02 15:39:13 +0200

    getAmmount

--- src/Loan.java
+++ src/Loan.java
@@ -2,22 +2,31 @@ public class Loan {
     private int ammount;
     private int duration;
     private float rate;
-    private float monthly;
+    private int monthly;
 
 
-    public void getAmmount(int duration, float monthly, float rate){
+    private float getTm(float rate){
+        return (float) Math.pow(1 + rate, 1f / 12f) - 1;
+    }
+
+    public int getAmmount(int duration, int monthly, float rate) {
+        float tm = getTm(rate);
+        return Math.round((float)(monthly * (
+                1 - (
+                        Math.pow(1 + tm,12f*duration/12)
+                )
+        )/tm));
+    }
+
+    public int getDuration(int ammount, int monthly, float rate) {
+        float tm = getTm(rate);
+    }
+
+    public int getMonthly(int ammount, int duration, float rate) {
 
     }
 
-    public void getDuration(int ammount, float monthly, float rate){
-
-    }
-
-    public void getMonthly(int ammount, int duration, float rate){
-
-    }
-
-    public float getTotal(){
+    public float getTotal() {
         return 0f;
     }
 }
