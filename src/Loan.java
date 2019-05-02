55301dac4cd34e38fd99e5e98db42c66cd520a0d
william <willef1999@gmail.com>
2019-05-02 15:55:52 +0200

    end Loan

--- src/Loan.java
+++ src/Loan.java
@@ -1,3 +1,5 @@
+import java.security.InvalidParameterException;
+
 public class Loan {
     private int ammount;
     private int duration;
@@ -5,28 +7,54 @@ public class Loan {
     private int monthly;
 
 
-    private float getTm(float rate){
+    private float getTm(float rate) {
         return (float) Math.pow(1 + rate, 1f / 12f) - 1;
     }
 
     public int getAmmount(int duration, int monthly, float rate) {
+        if (duration < 0 || monthly < 0 || rate < 0) {
+            throw new InvalidParameterException();
+        }
         float tm = getTm(rate);
-        return Math.round((float)(monthly * (
+        this.ammount = Math.round((float) (monthly * (
                 1 - (
-                        Math.pow(1 + tm,12f*duration/12)
+                        Math.pow(1 + tm, 12f * duration / 12)
                 )
-        )/tm));
+        ) / tm));
+        this.duration = duration;
+        this.monthly = monthly;
+        this.rate = rate;
+        return this.ammount;
     }
 
     public int getDuration(int ammount, int monthly, float rate) {
+        if (ammount < 0 || monthly < 0 || rate < 0) {
+            throw new InvalidParameterException();
+        }
         float tm = getTm(rate);
+        this.duration = Math.round((float) ((Math.log(monthly / (monthly - ammount * tm))) /
+                (Math.log(1 + tm))
+        ));
+        this.ammount = ammount;
+        this.monthly = monthly;
+        this.rate = rate;
+        return this.duration;
     }
 
     public int getMonthly(int ammount, int duration, float rate) {
+        if (ammount < 0 || duration < 0 || rate < 0) {
+            throw new InvalidParameterException();
+        }
+        float tm = getTm(rate);
 
+        this.monthly = Math.round((float) ((ammount * tm) / (1 - (1 / Math.pow(1 + tm, duration)))));
+        this.ammount = ammount;
+        this.duration = duration;
+        this.rate = rate;
+        return this.monthly;
     }
 
     public float getTotal() {
-        return 0f;
+        return monthly * duration;
     }
 }
